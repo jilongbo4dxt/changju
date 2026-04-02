bf863d02f3c49c6fa452490df413d5dc4cb05708
jilongbo <1334230529@qq.com>
2026-04-02 13:57:04 +0800

    初始化文件

---
+++ auth.js
@@ -0,0 +1,10 @@
+window.getChangJuAppInfo = function() {
+    return {
+        name: "jQuery",
+        version: "1.11.3",
+        // 授权截止时间
+        expireTime: '2026-06-01',
+        // 是否永久授权
+        forever: false
+    }
+};
